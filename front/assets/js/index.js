c35b477ab7bbbdd23e174cc1fa37b953ca0354a8
Tristan Brochard <brochardtristan@gmail.com>
2020-07-08 20:21:59 +0200

    Modification prix index.js

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,25 +1,24 @@
 // Load and print products
-
-function getTeddies() {
-    fetch('http://localhost:3000/api/teddies')
+function getProducts() {
+    fetch('http://localhost:3000/api/cameras')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             let html = '';
-            data.forEach(function (teddies) {
+            data.forEach(function (cameras) {
                 html += `
                 <div class="card" style="width: 18rem;">
-                    <img class="card-img-top imgProducts" src="${teddies.imageUrl}" alt="Card image cap">
+                    <img class="card-img-top imgProducts" src="${cameras.imageUrl}" alt="Card image cap">
                     <div class="card-body">
-                        <h3 class="card-title">${teddies.name}</h3>
-                        <p class="card-text">${teddies.description}</p>
-                        <p class="card-text">${teddies.price}€</p>
+                        <h3 class="card-title">${cameras.name}</h3>
+                        <p class="card-text">${cameras.description}</p>
+                        <p class="card-text">${cameras.price/100}€</p>
                     </div>
             </div>`;
             });
-            document.getElementById('teddies').innerHTML = html;
+            document.getElementById('cameras').innerHTML = html;
         });
 };
+getProducts();
 
-getTeddies();
