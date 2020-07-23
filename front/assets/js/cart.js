185b701d32f7c555467634474af4587f61232cff
Tristan Brochard <brochardtristan@gmail.com>
2020-07-23 23:08:51 +0200

    display products in cart

--- front/assets/js/cart.js
+++ front/assets/js/cart.js
@@ -1,6 +1,32 @@
-/*
-SI utilisateur a ajouter des produits au panier
-    AFFICHER la liste des produits dans le panier
-SINON
-    Panier vide, retourner à la page d'accueil
-*/
+displayCart();
+
+function displayCart() {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    let totalCart = localStorage.getItem('totalCost');
+    let productContainer = document.querySelector('#cart-list');
+    
+    
+
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(product => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <img class="card-img-top imgProducts" src="${product.imageUrl}" alt="Card image cap">
+                <span>${product.name}</span>            
+            </div>
+            <div class="price">${product.price / 100},00 €<div>
+            <div class="quantity">${product.inCart}</div>
+            <div class="total">${product.inCart * product.price / 100},00 €</div>
+            `;
+        }); 
+        productContainer.innerHTML += `
+         <div class="basket-total-container>
+            <h4 class="basket-total-title> Panier Total : </h4>
+            <span class="basket-total>${totalCart}</span>
+        </div>
+        `;
+        console.log(totalCart);
+    }
+}
--- front/assets/js/product.js
+++ front/assets/js/product.js
@@ -3,13 +3,15 @@ fetchProduct(url, getProductId()).then(function (product) {
   domElement.innerHTML = displayProduct(product, "main");
 
   // Ecoute au clic utilisateur
-  let addButton = document.querySelector("button");
-  addButton.addEventListener("click", function () {
+  let button = document.querySelector("button");
+  button.addEventListener("click", function () {
     getCartNumbers(product);
-    setCartProductId();
+    totalCost(product);
   });
 });
 
+
+// fonctionne asynchrone, va chercher la liste des produits
 async function fetchProduct(productUrl, productId) {
   const response = await fetch(productUrl + productId);
   return response.json();
@@ -43,14 +45,14 @@ function setCartProductId() {
   localStorage.setItem("cartProductId", getProductId());
 }
 
-
+// ajoute les produits cliqué au localStorage
 function setItems(product) {
 
   let cartItems = localStorage.getItem('productsInCart');
   cartItems = JSON.parse(cartItems);
 
   if (cartItems != null) {
-    
+
     if (cartItems[product._id] == undefined) {
       cartItems = {
         ...cartItems,
@@ -68,4 +70,17 @@ function setItems(product) {
   localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
+// récupère le prix de chaques items et les addtionnes pour renvoyer le total panier
+function totalCost(product) {
+  let cartCost = localStorage.getItem('totalCost');
+  
 
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem('totalCost', cartCost + product.price);
+  } else {
+    localStorage.setItem('totalCost', product.price);
+  }
+
+  
+}
