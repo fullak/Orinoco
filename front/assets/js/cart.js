850816c9cd92da92c97d4be872fb6d79a88f13ea
fullak <brochardtristan@gmail.com>
2020-08-10 16:11:34 +0200

    refactoring

--- front/assets/js/cart.js
+++ front/assets/js/cart.js
@@ -1,114 +1,101 @@
-if (getTotalProductsInCart() == 0) {
-  hideValidationOrder();
-  displayEmptyCartNotice();
+updatePageStatus();
+listenForCartCleanup();
+listenForChange();
+
+if (has('products')) {
+  fetchAjax(url).then(function (products) {
+    displayProducts(products);
+    listenForSubmission();
+  }); 
 }
 
-document.querySelector(".clear-cart").addEventListener("click", function () {
-  clear();
-  displayHeaderQtyOfProductInCart();
-  window.location.reload();
-});
 
-let form = document.querySelector("#checkout-form");
-const products = JSON.parse(localStorage.getItem("products"));
-let userCheckout = {
-  contact: {},
-  products,
-};
+function displayProducts(products) {
+  let html = '';
 
-const productInStorage = get("products");
-
-fetchAjax(url).then(function (products) {
-  const domElement = document.querySelector("#products");
-  let html = "";
-
-  productInStorage.forEach((value) => {
+  get('products').forEach((productId) => {
     products.forEach((product) => {
-      if (product._id != value) {
+      if (product._id != productId) {
         return null;
       } else {
         html += displayProduct(product, "cart");
       }
     });
-    domElement.innerHTML = html;
-    toValidate;
   });
-});
+  document.querySelector('#products').innerHTML = html;
+} 
 
-function displayEmptyCartNotice() {
-  $(".empty-cart").show();
+function updatePageStatus() {
+  if (getTotalProductsInCart() == 0) {
+    hideValidationOrder();
+    displayEmptyCartNotice();
+  }
 }
 
-function hideValidationOrder() {
-  $(".hide-form").hide();
-  $(".products-presentation").hide();
+function listenForCartCleanup() {
+  document.querySelector(".clear-cart").addEventListener("click", function () {
+    clear();
+    displayHeaderQtyOfProductInCart();
+    updatePageStatus();
+  });
 }
 
-form.querySelector("#validate").addEventListener("click", async (e) => {
-  e.preventDefault();
-
-  userCheckout.contact = {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    address: address.value,
-    city: city.value,
-    email: email.value,
-  };
-
-  let response = await postData(
-    "POST",
-    "http://localhost:3000/api/cameras/order",
-    userCheckout
-  ); // Envoie données au serveur
-
-  window.location = `./checkout.html?id=${response.orderId}&user=${firstName.value}`; // Redirige vers la page de confirmation de commande
-});
-
-const postData = async (method, url, dataElt) => {
-  const response = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method,
-    body: JSON.stringify(dataElt),
-  });
-  return await response.json();
-};
-
-const deleteCart = () => {
-  const panier = JSON.parse(localStorage.getItem("products"));
-  console.log("a");
-  console.log(panier);
-  for (key in panier) {
-    delete panier[key];
-    localStorage.setItem("products", JSON.stringify(panier));
-    console.log("b");
-    console.log(panier);
-    break;
-    //  location.reload();
-  }
-  console.log("c");
-  console.log(panier);
-};
-
-let toValidate = jQuery(".firstName, .lastName, .address, .city, .email"),
-  valid = false;
-$(".validate").hide();
-toValidate.keyup(function () {
-  if (jQuery(this).val().length > 0) {
-    jQuery(this).data("valid", true);
-  } else {
-    jQuery(this).data("valid", false);
-  }
-  toValidate.each(function () {
-    if (jQuery(this).data("valid") == true) {
-      valid = true;
-    } else {
-      valid = false;
+function listenForChange() {
+  document.getElementById('checkout-form').addEventListener('change', function (){
+    if (formIsValid()) {
+      enableSubmitButton();
+    }else {
+      disableSubmitButton();
     }
   });
-  if (valid === true) {
-    $(".validate").show();
-    $(".notValide").hide();
+}
+
+function listenForSubmission() {
+  document.getElementById("validate").addEventListener("click", function() {
+    let payload = {
+    contact: {
+      firstName: document.getElementById('firstName').value,
+      lastName: document.getElementById('lastName').value,
+      address: document.getElementById('address').value,
+      city: document.getElementById('city').value,
+      email: document.getElementById('email').value,
+    },
+    products: get('products')
+  };
+
+  fetch("http://localhost:3000/api/cameras/order", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(payload),
+    }).then(response => {
+      console.log(response)
+      window.location = `./checkout.html?id=${response.orderId}&user=${firstName.value}`; // Redirige vers la page de confirmation de commande
+    })
+  });
+}
+
+function isFormValid() {
+  return (isFirstNameValid() && isLastNameValid());
+}
+
+function isFirstNameValid() {
+  let firstName = document.getElementById('firstName').value;
+
+  if (firstName.length > 3) {
+    return true;
   }
-});
+
+  return false;
+}
+
+function isLastNameValid() {
+  let lastName = document.getElementById('lastName').value;
+
+  if (lastName.length > 2 && lastName.length < 200) {
+    return true;
+  }
+
+  return false;
+}
